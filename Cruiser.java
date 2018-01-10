a7c0c52cc6ab873114c1479d977f91e4b3eba4ee
StefanTan18 <stan3@stuy.edu>
2018-01-10 13:58:28 -0500

    added Ship subclasses

---
+++ Cruiser.java
@@ -0,0 +1,22 @@
+public class Cruiser extends Ship
+{
+
+    /*=============================================
+      default constructor
+      pre:  instance vars are declared
+      post: initializes instance vars.
+      =============================================*/
+    public Cruiser()
+    {
+	super();
+	health = 50;
+	attack = 60;
+	defense = 5;
+	movement = 6;
+	size = 3;
+    }
+
+    public void ability(){
+	System.out.println("Submarine")
+	    }
+}
---
+++ Destroyer.java
@@ -0,0 +1,22 @@
+public class Destroyer extends Ship
+{
+
+    /*=============================================
+      default constructor
+      pre:  instance vars are declared
+      post: initializes instance vars.
+      =============================================*/
+    public Destroyer()
+    {
+	super();
+	health = 50;
+	attack = 60;
+	defense = 5;
+	movement = 6;
+	size = 3;
+    }
+
+    public void ability(){
+	System.out.println("Submarine")
+	    }
+}
