28cb110fa3924af7270f06b1ab311455ed2e8393
LarWong <lwong7@stuy.edu>
2018-01-17 01:03:14 -0500

    added hit/miss

--- PlayerGrid.java
+++ PlayerGrid.java
@@ -4,10 +4,14 @@ public class PlayerGrid{
 
   private int[][] field = new int[15][15];
   private int[][] markup = new int[15][15];
+  private int[][] launches;
+  private boolean hasLaunches = false;
   private String _columns = "\t    00 01 02 03 04 05 06 07 08 09 10 11 12 13 14\n";
   private String[] _rows =
   {"00","01","02","03","04","05","06","07","08","09","10","11","12","13","14"};
   private int[][] restrictedArea = new int[20][20];
+
+
   private boolean hasRestrictions = false;
   private boolean placement = false;
 
@@ -52,14 +56,41 @@ public  String toString(){
   int ctr = 0;
   String s = "The Markup Grid: \n" + _columns;
 
-  for (int[] x : markup){
+  for (int x = 0; x < 15; x++){
     s += "\t" + _rows[ctr];
-    for (int y : x){
-      s += "  ~";
+
+    for (int y = 0; y < 15; y++){
+
+
+      if (hasLaunches == true){
+
+        for (int[] u : launches){
+          for (int r = 0; r < launches.length; r++){
+            for (int c = 0; c < 3; c++){
+
+              if (launches[r][0] == x && launches[r][1] == y && launches[r][3] == -1){
+                s += "O"; //its a hit
+                placement = true;
+              }else if (launches[r][0] == x && launches[r][1] == y){
+                s+= "X"; //its a miss
+                placement = true;
+              }
+            }
+          }
+        }
+
+      }
+
+      if (placement == false){
+        s += "  ~";
+      }
+      placement = false;
     }
     ctr += 1;
     s+= "\n";
   }
+
+  placement = false;
 
   s+= "\nYour Playing Field\n";
 
@@ -114,6 +145,11 @@ public  String toString(){
 public int[][] salvo(){
 
   int[][] salvo = new int[5][3];
+  for (int r = 0; r < salvo.length; r++){
+    for (int c = 0; c < 3; c ++){
+      salvo[r][c] = -1;
+    }
+  }
 
   for (int shipNum = 0; shipNum < 5; shipNum++) {
     // Coordinates
@@ -125,9 +161,9 @@ public int[][] salvo(){
       salvo[shipNum][0] = Keyboard.readInt();
 
       while (salvo[shipNum][0] > 14 || salvo[shipNum][0] < 0) {
-          System.out.println("INVALID ROW NUMBER");
-          System.out.println("Enter the row number (0-14):");
-          salvo[shipNum][0] = Keyboard.readInt();
+        System.out.println("INVALID ROW NUMBER");
+        System.out.println("Enter the row number (0-14):");
+        salvo[shipNum][0] = Keyboard.readInt();
 
       }
 
@@ -135,9 +171,9 @@ public int[][] salvo(){
       salvo[shipNum][1] = Keyboard.readInt();
 
       while (salvo[shipNum][1] == - 1 || salvo[shipNum][1] > 14 || salvo[shipNum][1] < 0) {
-          System.out.println("INVALID COLUMN NUMBER");
-          System.out.println("Enter the column number (0-14):");
-          salvo[shipNum][1] = Keyboard.readInt();
+        System.out.println("INVALID COLUMN NUMBER");
+        System.out.println("Enter the column number (0-14):");
+        salvo[shipNum][1] = Keyboard.readInt();
 
 
       }
@@ -146,14 +182,55 @@ public int[][] salvo(){
       System.out.println("\n\n");
       // Debugging
       // System.out.println(shipNum);
-        //System.out.print(salvo[shipNum][0]);
-        //System.out.print(salvo[shipNum][1]);
-       // System.out.print(salvo[shipNum][2]);
+      //System.out.print(salvo[shipNum][0]);
+      //System.out.print(salvo[shipNum][1]);
+      // System.out.print(salvo[shipNum][2]);
     }
   }
 
   return salvo;
 }
+
+public void hitsOrMiss(int[][] salvo){
+
+
+  int alive = 0;
+  for (int[] r : salvo){
+    if (r[0] != -1 && r[1] != -1){
+      alive++;
+    }
+  }
+
+  int[][] shots = new int[alive][3];
+  for (int r = 0; r < alive; r++){
+    for (int c = 0; c < 3; c++){
+      shots[r][c] = salvo[r][c];
+    }
+  }
+
+  for (Ship x : ships){
+    if (x.alive()){
+      int[][] shipCoords = x.getLocation();
+      for (int r = 0; r < alive; r++){
+        if (shipCoords[r][0] == shots[r][0] && shipCoords[r][1] == shots[r][1]){
+          for (int c = 0; c < 3; c++){
+            x.updateHealth(shots[r][2]);
+            shots[r][2] = -1;
+          }
+        }
+      }
+
+    }
+  }
+  hasLaunches = true;
+  launches = shots;
+}
+
+
+
+
+
+
 public void printShips(){
   for (Ship z : ships){
     System.out.println("-----------------------------\n");
@@ -186,89 +263,89 @@ public boolean checkArea(int[] coord){
 
   for (Ship x : ships){
     if (x.alive()){
-    int[][] shipCoords = x.getLocation();
-    for (int[] loc : shipCoords){
-      if (loc[0] == coord[0] && loc[1] == coord[1]){
-        System.out.println("\nINVALID PLACEMENT (space occupied)");
-        return false;
-      }
+      int[][] shipCoords = x.getLocation();
+      for (int[] loc : shipCoords){
+        if (loc[0] == coord[0] && loc[1] == coord[1]){
+          System.out.println("\nINVALID PLACEMENT (space occupied)");
+          return false;
+        }
 
-    }
-  }}
+      }
+    }}
 
-  if (hasRestrictions){
-    for (int[] x : restrictedArea)
-    {
-      if (x[0] == coord[0] && x[1] == coord[1]){
-        return false;
+    if (hasRestrictions){
+      for (int[] x : restrictedArea)
+      {
+        if (x[0] == coord[0] && x[1] == coord[1]){
+          return false;
+        }
       }
     }
+
+    return true;
   }
 
-  return true;
-}
+  public boolean setLocation(String shipName, int[] coord, String direction) {
+    for (Ship s: ships) {
+      if (s.getName().equals(shipName)){
+        currentShip = s;
+      }
+    }
+    // for (int v: coord) {
+    //   System.out.println(v);
+    // }
 
-public boolean setLocation(String shipName, int[] coord, String direction) {
-  for (Ship s: ships) {
-    if (s.getName().equals(shipName)){
-      currentShip = s;
+    int[][] setHere = new int[currentShip.getSize()][2];
+    int[] newCoords = new int[2];
+    //checking if ship is placed on a restricted area
+    if (checkArea(coord) == false){
+      return false;
     }
-  }
-  // for (int v: coord) {
-  //   System.out.println(v);
-  // }
+    for (int x = 0; x < currentShip.getSize(); x++){
+      if (direction.equals("North")){
+        newCoords[0] = coord[0] - x;
+        newCoords[1] = coord[1];
+        if (checkArea(newCoords) == true){
+          setHere[x][0] = newCoords[0];
+          setHere[x][1] = newCoords[1];
+        }else{
+          return false;
+        }
+      }
+      if (direction.equals("South")){
+        newCoords[0] = coord[0] + x;
+        newCoords[1] = coord[1];
+        if (checkArea(newCoords) == true){
+          setHere[x][0] = newCoords[0];
+          setHere[x][1] = newCoords[1];
+        }else{
+          return false;
+        }
+      }
+      if (direction.equals("East")){
+        newCoords[0] = coord[0];
+        newCoords[1] = coord[1] + x;
+        if (checkArea(newCoords) == true){
+          setHere[x][0] = newCoords[0];
+          setHere[x][1] = newCoords[1];
+        }else{
+          return false;
+        }
+      }
+      if (direction.equals("West")){
+        newCoords[0] = coord[0];
+        newCoords[1] = coord[1] - x;
+        if (checkArea(newCoords) == true){
+          setHere[x][0] = newCoords[0];
+          setHere[x][1] = newCoords[1];
+        }else{
+          return false;
+        }
+      }
+    }
 
-  int[][] setHere = new int[currentShip.getSize()][2];
-  int[] newCoords = new int[2];
-  //checking if ship is placed on a restricted area
-  if (checkArea(coord) == false){
-    return false;
-  }
-  for (int x = 0; x < currentShip.getSize(); x++){
-    if (direction.equals("North")){
-      newCoords[0] = coord[0] - x;
-      newCoords[1] = coord[1];
-      if (checkArea(newCoords) == true){
-        setHere[x][0] = newCoords[0];
-        setHere[x][1] = newCoords[1];
-      }else{
-        return false;
-      }
-    }
-    if (direction.equals("South")){
-      newCoords[0] = coord[0] + x;
-      newCoords[1] = coord[1];
-      if (checkArea(newCoords) == true){
-        setHere[x][0] = newCoords[0];
-        setHere[x][1] = newCoords[1];
-      }else{
-        return false;
-      }
-    }
-    if (direction.equals("East")){
-      newCoords[0] = coord[0];
-      newCoords[1] = coord[1] + x;
-      if (checkArea(newCoords) == true){
-        setHere[x][0] = newCoords[0];
-        setHere[x][1] = newCoords[1];
-      }else{
-        return false;
-      }
-    }
-    if (direction.equals("West")){
-      newCoords[0] = coord[0];
-      newCoords[1] = coord[1] - x;
-      if (checkArea(newCoords) == true){
-        setHere[x][0] = newCoords[0];
-        setHere[x][1] = newCoords[1];
-      }else{
-        return false;
-      }
-    }
-  }
-  
-  /*
-  for (int v: newCoords) {
+    /*
+    for (int v: newCoords) {
     System.out.println(v);
   }
   */
