8f73e00ed5a4f29a91a5e87de377eac7456f0aa7
LarWong <lwong7@stuy.edu>
2018-01-17 00:00:56 -0500

    fix typos

--- Ship.java
+++ Ship.java
@@ -1,5 +1,4 @@
 public abstract class Ship{
-<<<<<<< HEAD
   private boolean isAlive = false;
   protected double health;
   protected int attack, defense, movement, size;
@@ -79,77 +78,4 @@ public abstract class Ship{
       isAlive = false;
   }
 
-=======
-    private boolean isAlive = false;
-    protected double health;
-    protected int attack, defense, movement, size;
-    protected int[][] coords;
-    protected String symbol;
-    protected String name;
-
-    public boolean alive(){
-	return isAlive;
-    }
-
-    public void setAlive(){
-	isAlive = true;
-    }
-
-    public String getName(){
-	return name;
-    }
-
-    public String getSymbol(){
-	return symbol;
-    }
-
-    public double getHealth(){
-	return health;
-    }
-
-    public int getAttack(){
-	return attack;
-    }
-
-    public int getDefense(){
-	return defense;
-    }
-
-    public int getMovement(){
-	return movement;
-    }
-
-    public int getSize(){
-	return size;
-    }
-
-    public int[][] getLocation(){
-	return coords;
-    }
-
-    //  public abstract void setLocation(int[][] coords);
-
-    public boolean setLocation(int[][] loc){
-	for (int r = 0; r < size; r++){
-	    for (int c = 0; c < 2; c++){
-		coords[r][c] =loc[r][c];
-	    }
-	}
-	return true;
-
-    }
-
-    public int shoot(){
-	int damage = (int)(Math.random() * 5 * attack);
-	if ( damage < 0 )
-	    damage = 0;
-	return damage;
-    }
-
-    public void updateHealth(int damage){
-	health = health - (damage - defense);
-	if (health <= 0)
-	    isAlive = false;
-    }
->>>>>>> dbbefaf43373e9da9e61326fbee8021269c05824
 }
